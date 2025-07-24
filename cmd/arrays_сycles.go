46f0896e803f064abee46be96540b626aa3afb82
AlexRijikov <arijikov88@gmail.com>
2025-07-24 16:35:33 +0300

    Added to the file arrays_cycles array problems

--- cmd/arrays_сycles.go
+++ cmd/arrays_сycles.go
@@ -1,82 +1,159 @@
 package main
+//const WAVE = 10
 
-import (
-	"fmt"
-)
+// func WorkingWithAnArray() {
 
-const WAVE = 10
+// Синтаксис оголошення масиву
 
-func WorkingWithAnArray() {
+// 	arr := [3]int{1, 2, 3} //Фіксований розмір масиву
 
-	// Синтаксис оголошення масиву
+// 	fmt.Println(arr)
 
-	arr := [3]int{1, 2, 3} //Фіксований розмір масиву
+// 	//Ініціалізація масивів
+// 	arrA := [5]int{}              //не ініціалізовано
+// 	arrB := [5]int{1, 2}          //частково ініціалізовано
+// 	arrC := [5]int{1, 2, 3, 4, 5} //повністю ініціалізовано
 
-	fmt.Println(arr)
+// 	fmt.Println(arrA)
+// 	fmt.Println(arrB)
+// 	fmt.Println(arrC)
 
-	//Ініціалізація масивів
-	arrA := [5]int{}              //не ініціалізовано
-	arrB := [5]int{1, 2}          //частково ініціалізовано
-	arrC := [5]int{1, 2, 3, 4, 5} //повністю ініціалізовано
+// 	//Практика: створити 3 різних масивів
 
-	fmt.Println(arrA)
-	fmt.Println(arrB)
-	fmt.Println(arrC)
+// 	arrCar := [5]string{"BMW", "ZAZ", "Ford", "Opel", "Renault"}
 
-	//Практика: створити 3 різних масивів
+// 	arrTemp := [4]float32{12.2, 13.5, 14.3, 22.6}
 
-	arrCar := [5]string{"BMW", "ZAZ", "Ford", "Opel", "Renault"}
+// 	fmt.Println(arrCar[2]) //Доступ до елементів за індексом
 
-	arrTemp := [4]float32{12.2, 13.5, 14.3, 22.6}
+// 	fmt.Println(arrTemp)
 
-	fmt.Println(arrCar[2]) //Доступ до елементів за індексом
+// 	// Робота з елементами масиву
 
-	fmt.Println(arrTemp)
+// 	arrSchool := [8]string{"Class1", "Class2", "Class3", "Class4", "Class5", "Class6", "Class7", "Class8"}
 
-	// Робота з елементами масиву
+// 	arrSchool[2] = "NoClass" //Зміна значень елементів
 
-	arrSchool := [8]string{"Class1", "Class2", "Class3", "Class4", "Class5", "Class6", "Class7", "Class8"}
+// 	fmt.Println(arrSchool)
 
-	arrSchool[2] = "NoClass" //Зміна значень елементів
+// 	//Ітерація через масив (for, range)
 
-	fmt.Println(arrSchool)
+// 	arrProblem := [5]string{"Engine", "Chassis", "Body", "Electricity", "Salon"}
 
-	//Ітерація через масив (for, range)
+// 	fmt.Println(len(arrProblem), "What could be the car's faults: ", arrProblem) //len() функція для масивів для визначення довжини масиву
 
-	arrProblem := [5]string{"Engine", "Chassis", "Body", "Electricity", "Salon"}
+// 	numbers := []int{10, 20, 30, 40, 50}
 
-	fmt.Println(len(arrProblem), "What could be the car's faults: ", arrProblem) //len() функція для масивів для визначення довжини масиву
+// 	fmt.Println("Last Value:", AlwaysGetLastValue(numbers))
 
+// 	ConstValue()
 
-	numbers := []int{10, 20, 30, 40, 50}
+// }
 
-	fmt.Println("Last Value:", AlwaysGetLastValue(numbers)) 
+// // Практика: через цикли додати значення 10 до кожного елементу масиву через константу
 
-	ConstValue()
+// func ConstValue() {
+
+// 	arrValue := [6]int{1, 2, 3, 4, 5, 6}
+
+// 	if len(arrValue) == 0 {
+// 		return
+// 	}
+
+// 	for i := 0; i < len(arrValue); i++ {
+// 		arrValue[i] += WAVE
+// 	}
+
+// 	fmt.Println("New Valume ", arrValue)
+
+// }
+
+// // Вивести останній елемент масиву в окремій функції
+
+// func AlwaysGetLastValue(numbers []int) int {
+
+// 	return numbers[len(numbers)-1]
+// }
+
+//func printNameFerstName(fname string) {
+
+// fmt.Println("Hello", fname, "Ryzhykov")
+
+//}
+
+// Cтворив функцію сортування  яка приймає слайс інтів та повертає слайс інтів
+func SortSlice(numbers []int) []int {
+
+	// Ствоив зманну ар за допомогою фунції make яка має переметр цілих чисил та довжину слайсу numbers (5)
+	arr := make([]int, len(numbers))
+	// За допомогою функії copy роблю комію слайса numbers в слайс arr
+	copy(arr, numbers)
+
+	// створюю допоміжну змінну n яка містить довжину скопійованого слайсу
+	n := len(arr)
+	// Через цикл фор записую що після кожного проходу  найбільше число записується в кінець
+	// тому з кожним проходом треба робити на -1 (н-1) перевіру менше
+	for i := 0; i < n-1; i++ {
+		// внутрений цикл проходить по усім не пройденим цифрам
+		// а умова (n-і-1) що треба пропустити вже відсортовані цифри
+		for j := 0; j < n-i-1; j++ {
+			// Певеріряємо два сусідні елементи якщо  ліве число більше за праве то міняемо їх місцями
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+	// ключове слово яке повертає результат значення arr
+	return arr
+
 }
+// Функція яка приймає слайс чисел та повертає їх суму
+func SumSlice(numbers []int) int {
 
-// Практика: через цикли додати значення 10 до кожного елементу масиву через константу
-
-func ConstValue() {
-
-	arrValue := [6]int{1, 2, 3, 4, 5, 6}
-
-	if len(arrValue) == 0 {
-		return
+	//Створив змінну сума и дав їй зачення 0
+	sum := 0
+	//  За допомогою циклу та допоміжного слова range перебераю слайс numbers
+	for _, value := range numbers {
+		// в змінну сума додаю значення зміної value конен раз циклу
+		sum += value
 	}
+	// Повертаю значення циклу (суму)
+	return sum
 
-	for i := 0; i < len(arrValue); i++ {
-		arrValue[i] += WAVE
+}
+//Функція яка рахує тільки парні цифри
+func SumEvenSlice(numbers []int) int {
+
+	sumEven := 0
+
+	for _, velue := range numbers {
+		if velue%2 == 0 {
+			sumEven += velue
+		}
 	}
+	return sumEven
+}
+func MinVelues(nambers []int) int{
 
-	fmt.Println("New Valume ", arrValue)
+	minVelue := nambers[0] 
 
-}	
+	for  i := 1;i< len(nambers);i++{
+		if nambers[i]< minVelue{
+			minVelue = nambers[i]
+ 
+		}
+	}
+	return minVelue }
 
-	// Вивести останній елемент масиву в окремій функції
+func MaxVelues( nambers []int) int {
+	
+    maxVelue := nambers[0]
 
-func AlwaysGetLastValue(numbers []int) int {
-
-	return numbers[len(numbers)-1] 
+	for i := 1;i < len(nambers);i++{
+		if nambers[i]>maxVelue{
+			maxVelue = nambers[i]
+		}
+	}
+	return maxVelue
 
 }
--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,52 @@
 package main
 
+import "fmt"
+
 func main() {
 
 	//ScanUserInfo() // Робота з користувачем
 
-	//PrintZeroValues() // Робота з нульовими значеннями 
+	//PrintZeroValues() // Робота з нульовими значеннями
 
 	//PrintVarbsFormatting() //  Робота дієслів форматування з ЗАГАЛЬНИМИ типа
 
-	//VarbsForWholeNmders() //  Робота дієслів форматування з Цілими числами 
+	//VarbsForWholeNmders() //  Робота дієслів форматування з Цілими числами
 
 	// VarbsForString()  // Робота дієслів форматування з Страками
 
-	// LogicalVarbs()  // Логічні дієслова  (так само як при використанні %v)  
+	// LogicalVarbs()  // Логічні дієслова  (так само як при використанні %v)
 
 	// VarbsForFloat()// Дієслова форматування Float
 
-	WorkingWithAnArray() // Робота з масивами
+	//  WorkingWithAnArray() // Робота з масивами
 
-	
+	//  printNameFerstName("Katya")
+	//  printNameFerstName("Alex")
+	//  printNameFerstName("Monika")
 
+	// Створив слайс інтів який має різні числоа
+	numbers := []int{9, 3, 22, 12, 7}
+
+	// Створив зміну сорт яка дорівнює  функцію сортслайс та має в собі параметр цифр
+	sortNumber := SortSlice(numbers)
+
+	sumNumbers := SumSlice(numbers)
+
+	sumEvenSlice := SumEvenSlice(numbers)
+
+	minVelues := MinVelues(numbers)
+
+	maxVelues := MaxVelues(numbers)
+
+	// Вивід слайсів до сортування та після сортування
+	fmt.Println("Original slice: ", numbers)
+	fmt.Println("Sorted slice:", sortNumber)
+	// Вивід суми всях чисел 
+	fmt.Println("Sum:",sumNumbers)
+	// Вивід суми парних цифр
+	fmt.Println("Sum Even Numbers: ",sumEvenSlice)
+// Вивід мінам і макс значення 
+
+fmt.Println("Min Velues :",minVelues)
+fmt.Println("Max Velues :",maxVelues)
 }
